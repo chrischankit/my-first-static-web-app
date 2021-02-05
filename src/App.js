5988c00787bd4a84f8686d962d0e44a4c6062b45
Chris Chan <chrischankit@gmail.com>
2021-02-04 22:59:44 -0800

    All good!

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const value = 'World from chrischankit 02/04/2021';
+  return <div><H1>Hello {value}</H1></div>;
 }
 
 export default App;
